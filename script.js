993f8d7cef02bebf0807ef92c30b69cb811c8eca
LuluLampe <pechokent2112@gmail.com>
2025-07-06 14:29:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const container = document.getElementById('pokemon-container');
+
+for (let i = 1; i <= 151 ; i++) {
+    const name = getPokemonName(i);
+    const box = document.createElement('div');
+    box.className = 'box';
+
+    const img = document.createElement('img');
+    img.src = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${i.toString().padStart(3, '0')}.png`;
+    img.alt = name;
+    img.width = 200;
+    img.height = 200;
+
+    const p = document.createElement('p');
+    p.textContent = name;
+    
+    box.appendChild(img);
+    box.appendChild(p);
+
+    container.appendChild(box);
+
+    console.log(getPokemonName(i));
+
+}
+
+function getPokemonName (number) {
+    const names = [
+  "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+  "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
+  "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot",
+  "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok",
+  "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina",
+  "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable",
+  "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat",
+  "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat",
+  "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck",
+  "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag",
+  "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop",
+  "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool",
+  "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash",
+  "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch’d", "Doduo",
+  "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder",
+  "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee",
+  "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute",
+  "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung",
+  "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela",
+  "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu",
+  "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar",
+  "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto",
+  "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte",
+  "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno",
+  "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo",
+  "Mew"
+];
+    return names[number - 1] || `Pokemon #${number}`;
+}
